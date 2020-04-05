30177957922de437fa77eabc17f55c8374aabdd9
Fabio Papa <fabtheman@gmail.com>
2020-04-04 18:53:40 -0700

    Update TodoList to use Struct instead of MultiDict

--- todo.ex
+++ todo.ex
@@ -1,11 +1,28 @@
 defmodule TodoList do
-  def new(), do: MultiDict.new()
+  defstruct auto_id: 1, entries: %{}
 
-  def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title)
+  def new(), do: %TodoList{}
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    new_entries =
+      Map.put(
+        todo_list.entries,
+        todo_list.auto_id,
+        entry
+      )
+
+    %TodoList{
+      todo_list
+      | entries: new_entries,
+        auto_id: todo_list.auto_id + 1
+    }
   end
 
-  def entries(todo_list, entry) do
-    MultiDict.get(todo_list, entry.date, entry)
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
   end
 end
